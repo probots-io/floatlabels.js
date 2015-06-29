942ed5b8b71f77af8a08ccb8d93082a9a2982b30
Mathieu Rosser <rossmat@users.noreply.github.com>
2015-06-29 14:48:38 +0200

    Input event to detect autocompletion

--- floatlabels.js
+++ floatlabels.js
@@ -75,7 +75,7 @@
                 if( !settings.slideInput ) {                    
                     thisElement.css({ 'padding-top' : this.inputPaddingTop });
                 }
-                thisElement.on('keyup blur change', function( e ) {
+                thisElement.on('keyup blur change input', function( e ) {
                     self.checkValue( e );
                 });
                 thisElement.on('blur', function() { thisElement.prev('label').css({ 'color' : self.settings.blurColor }); });
